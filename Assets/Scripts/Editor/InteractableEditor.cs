6eefbddc19a2916554a361d01ad775a5c15eb401
p-sun <paige.sun.dev@gmail.com>
2023-01-08 02:28:37 -0500

    feat: Add EventOnlyInteractable to set UnityEvents easily without any scripts. Add a cube to change door color as example.

--- Assets/Scripts/Editor/InteractableEditor.cs
+++ Assets/Scripts/Editor/InteractableEditor.cs
@@ -6,16 +6,34 @@ public class InteractableEditor : Editor
     public override void OnInspectorGUI()
     {
         Interactable interactable = (Interactable)target;
-        base.OnInspectorGUI();
-
         InteractionEvent component = interactable.GetComponent<InteractionEvent>();
-        if (interactable.useEvents && component == null)
+
+        if (target.GetType() == typeof(EventOnlyInteractable))
         {
-            interactable.gameObject.AddComponent<InteractionEvent>();
+            // Make Inspector GUI b/c we don't call `base.OnInspectorGUI();`
+            interactable.promptMessage = EditorGUILayout.TextField("Prompt Message", interactable.promptMessage);
+            EditorGUILayout.HelpBox("EventOnlyInteractable can ONLY use UnityEvents.", MessageType.Info);
+
+            if (component == null)
+            {
+                interactable.useEvents = true;
+                interactable.gameObject.AddComponent<InteractionEvent>();
+            }
         }
-        else if (!interactable.useEvents && component != null)
+        else
         {
-            DestroyImmediate(component);
+            base.OnInspectorGUI();
+
+            if (interactable.useEvents)
+            {
+                if (component == null)
+                    interactable.gameObject.AddComponent<InteractionEvent>();
+            }
+            else
+            {
+                if (component != null)
+                    DestroyImmediate(component);
+            }
         }
     }
 }
